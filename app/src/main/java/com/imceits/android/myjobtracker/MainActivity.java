f37a728ff0d04861c3ac085659a2f9b7fb1c205a
aungtuntun <aungtuntun2012@gmail.com>
2020-02-11 23:09:47 +0800

    Screen shots of processes

--- app/src/main/java/com/imceits/android/myjobtracker/MainActivity.java
+++ app/src/main/java/com/imceits/android/myjobtracker/MainActivity.java
@@ -5,7 +5,7 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -21,35 +21,42 @@ public class MainActivity extends AppCompatActivity  {
     NavController navController;
     JobsViewModel mViewModel;
     Toolbar toolbar;
+    GoogleSignInAccount account;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        appBarConfiguration = new AppBarConfiguration.Builder().build();
+
+        mViewModel = new ViewModelProvider(this).get(JobsViewModel.class);
+        // removing back navigation arrow from toolbar
+        appBarConfiguration = new AppBarConfiguration.Builder(R.id.signin).build();
+
         navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
      //   NavigationUI.setupActionBarWithNavController(this, navController);
-        NavigationUI.setupWithNavController(toolbar, navController);
 
         navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            // hide default toolbar
             if (destination.getId() == R.id.jobs) {
                 toolbar.setVisibility(View.GONE);
             }else {
                 toolbar.setVisibility(View.VISIBLE);
             }
         });
-        mViewModel = ViewModelProviders.of(this).get(JobsViewModel.class);
-
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        account = GoogleSignIn.getLastSignedInAccount(this);
         if (account != null) {
             mViewModel.updateCurrentUserPhotoUri(account.getPhotoUrl());
             mViewModel.updateCurrentUserName(account.getDisplayName());
+        }else {
+            // go to sign in fragment if account is not sign in
+            navController.navigate(R.id.signin);
         }
     }
 
